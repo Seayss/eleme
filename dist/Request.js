e9718d187607f70f4befc262b5ac77db08725948
zhuweiyou <i@zhuweiyou.com>
2018-09-19 14:53:10 +0800

    add parseCookie(), cleanCookie()

---
+++ dist/Request.js
@@ -0,0 +1,92 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var querystring = require("querystring");
+var axios_1 = require("axios");
+var Request = /** @class */ (function () {
+    function Request(proxy) {
+        this.client = axios_1.default.create({
+            proxy: proxy,
+            baseURL: "https://h5.ele.me",
+            method: "POST",
+            headers: {
+                referer: "https://h5.ele.me/hongbao/",
+                origin: "https://h5.ele.me",
+                "x-shard": Request.createXShared(),
+                "user-agent": "Mozilla/5.0 (Linux; Android 7.0; MIX Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.132 MQQBrowser/6.2 TBS/044004 Mobile Safari/537.36 V1_AND_SQ_7.5.0_794_YYB_D QQ/7.5.0.3430 NetType/WIFI WebP/0.3.0 Pixel/1080"
+            }
+        });
+    }
+    /**
+     * 底层请求，可以做一些统一的请求前后处理
+     * @param {string} method 请求方式
+     * @param {string} url 请求地址
+     * @param {*} data 请求数据
+     * @param {AxiosRequestConfig} config 其它配置
+     * @returns {Promise<AxiosResponse>}
+     * @private
+     */
+    Request.prototype.send = function (method, url, data, config) {
+        return __awaiter(this, void 0, void 0, function () {
+            var e_1;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        _a.trys.push([0, 2, , 3]);
+                        return [4 /*yield*/, this.client[method](url, querystring.stringify(data), config)];
+                    case 1: return [2 /*return*/, _a.sent()];
+                    case 2:
+                        e_1 = _a.sent();
+                        return [2 /*return*/, e_1.response];
+                    case 3: return [2 /*return*/];
+                }
+            });
+        });
+    };
+    /**
+     * 生成请求必备的 eosid，领取红包时传入，其他时候可以不传，使用默认提供的
+     * @param {string} sn 红包链接标识
+     * @returns {string}
+     */
+    Request.createXShared = function (sn) {
+        if (sn === void 0) { sn = "29e47b57971c1c9d"; }
+        return "eosid=" + parseInt(sn, 16);
+    };
+    return Request;
+}());
+exports.Request = Request;
+//# sourceMappingURL=Request.js.map
